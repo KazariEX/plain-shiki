c1cfd55b0b9e54a92aa118dbf1c2a89ee155b098
KazariEX <1364035137@qq.com>
2024-10-06 17:42:33 +0800

    chore(playground): use JS engine

--- playground/app/composables/usePlainShiki.ts
+++ playground/app/composables/usePlainShiki.ts
@@ -1,4 +1,4 @@
-import { type BundledLanguage, createHighlighter } from "shiki";
+import { type BundledLanguage, createHighlighter, createJavaScriptRegexEngine } from "shiki";
 import { createPlainShiki, type CreatePlainShikiReturns, type MountPlainShikiOptions } from "../../../src";
 
 export type UsePlainShikiOptions = Omit<MountPlainShikiOptions, "lang"> & {
@@ -25,7 +25,8 @@ export default function(el: MaybeRefOrGetter<HTMLElement | null>, options: UsePl
     tryOnMounted(async () => {
         const shiki = await createHighlighter({
             langs: ["html", "css", "js", "ts"],
-            themes: Object.values(options.themes ?? {}).filter((theme) => theme !== void 0)
+            themes: Object.values(options.themes ?? {}).filter((theme) => theme !== void 0),
+            engine: createJavaScriptRegexEngine()
         });
 
         plain = createPlainShiki(shiki);
